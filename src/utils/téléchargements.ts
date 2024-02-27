9f22de60f343f524f68f52115545728ed0208cb4
julienmalard <julien.malard@mail.mcgill.ca>
2024-02-27 16:01:13 +0100

    Téléchargement

--- src/utils/téléchargements.ts
+++ src/utils/téléchargements.ts
@@ -1,39 +1,34 @@
-import axios from "axios";
-import platform from "platform";
-import semver from "semver";
+import axios from 'axios';
+import platform from 'platform';
+import semver from 'semver';
 
 const extentionCompatible = (ext: string): boolean => {
-  if (platform.os?.family?.includes("Ubuntu")) {
-    return ext === "deb";
-  } else if (platform.os?.family?.includes("OS X")) {
-    return ext === "dmg";
-  } else if (platform.os?.family?.includes("Windows")) {
-    return ext === "exe";
+  if (platform.os?.family?.includes('Ubuntu')) {
+    return ext === 'deb';
+  } else if (platform.os?.family?.includes('OS X')) {
+    return ext === 'dmg';
+  } else if (platform.os?.family?.includes('Windows')) {
+    return ext === 'exe';
   } else {
     return false;
   }
 };
 
-export const plateforme = ():
-  | "windows"
-  | "mac"
-  | "linux"
-  | "téléphoneOuTablette"
-  | undefined => {
-  if (platform.os?.family?.includes("Ubuntu")) {
-    return "linux";
-  } else if (platform.os?.family?.includes("OS X")) {
-    return "mac";
-  } else if (platform.os?.family?.includes("Windows")) {
-    return "windows";
-  } else if (platform.os?.family! in ["Android", "iOS", "Windows Phone"]) {
-    return "téléphoneOuTablette";
+export const plateforme = (): 'windows' | 'mac' | 'linux' | 'téléphoneOuTablette' | undefined => {
+  if (platform.os?.family?.includes('Ubuntu')) {
+    return 'linux';
+  } else if (platform.os?.family?.includes('OS X')) {
+    return 'mac';
+  } else if (platform.os?.family?.includes('Windows')) {
+    return 'windows';
+  } else if (platform.os?.family! in ['Android', 'iOS', 'Windows Phone']) {
+    return 'téléphoneOuTablette';
   }
 };
 
 export const surOrdi = () => {
   const so = plateforme();
-  return so && so in ["windows", "mac", "linux"];
+  return so && so in ['windows', 'mac', 'linux'];
 };
 
 export type auteurGitHub = {
@@ -95,15 +90,14 @@ type publicationGitHub = {
 };
 
 export const obtLienTéléchargement = async (): Promise<string | undefined> => {
-  const jsonTéléchargements = (await axios.get(IPA_TÉLÉCHARGEMENTS))
-    .data as publicationGitHub[];
+  const jsonTéléchargements = (await axios.get(IPA_TÉLÉCHARGEMENTS)).data as publicationGitHub[];
   let versionPlusRécente: string | undefined = undefined;
   let urlTéléchargement: string | undefined = undefined;
   for (const t of jsonTéléchargements) {
     const version = t.name;
     if (!versionPlusRécente || semver.gt(version, versionPlusRécente)) {
       for (const fichier of t.assets) {
-        const ext = fichier.name.split(".").pop();
+        const ext = fichier.name.split('.').pop();
         if (ext && extentionCompatible(ext)) {
           versionPlusRécente = version;
           urlTéléchargement = fichier.browser_download_url;
@@ -115,43 +109,38 @@ export const obtLienTéléchargement = async (): Promise<string | undefined> => 
 };
 
 export const correspExtentions = {
-  linux: ["deb", "AppImage"],
-  mac: ["dmg"],
-  windows: ["exe"],
-  source: ["zip", "gz"],
+  linux: ['deb', 'AppImage'],
+  mac: ['dmg'],
+  windows: ['exe'],
+  source: ['zip', 'gz'],
 } as const;
 
 const extensionsPossibles = Object.values(correspExtentions).flat();
-
+export type TypeSO = keyof typeof correspExtentions;
+export type TypeExtention = (typeof correspExtentions)[keyof typeof correspExtentions][number];
 export type InfoTéléchargement = {
   version: string;
   so: keyof typeof correspExtentions;
   url: string;
 };
-export const obtTousLesTéléchargements = async (): Promise<
-  InfoTéléchargement[]
-> => {
-  const jsonTéléchargements = (await axios.get(IPA_TÉLÉCHARGEMENTS))
-    .data as publicationGitHub[];
+export const trouverSO = ({ext}: {ext: TypeExtention}): TypeSO | undefined => {
+  return Object.keys(correspExtentions).find(cl =>
+    (correspExtentions[cl as TypeSO] as unknown as TypeExtention[]).includes(ext),
+  ) as TypeSO;
+};
+
+export const obtTousLesTéléchargements = async (): Promise<InfoTéléchargement[]> => {
+  const jsonTéléchargements = (await axios.get(IPA_TÉLÉCHARGEMENTS)).data as publicationGitHub[];
   const téléchargements: InfoTéléchargement[] = [];
   for (const t of jsonTéléchargements) {
     const version = t.name;
     for (const fichier of t.assets) {
-      const ext = fichier.name.split(".").pop();
-      console.log({ ext });
-      if (
-        ext &&
-        extensionsPossibles.includes(
-          ext as (typeof extensionsPossibles)[number],
-        )
-      ) {
+      const ext = fichier.name.split('.').pop() as TypeExtention;
+      const so = trouverSO({ext});
+      if (ext && so) {
         téléchargements.push({
           version,
-          so: Object.keys(correspExtentions).find((cl) =>
-            correspExtentions[cl as keyof typeof correspExtentions].includes(
-              ext as (typeof extensionsPossibles)[number],
-            ),
-          ),
+          so,
           url: fichier.browser_download_url,
         });
       }
@@ -159,7 +148,5 @@ export const obtTousLesTéléchargements = async (): Promise<
   }
   return téléchargements;
 };
-export const IPA_TÉLÉCHARGEMENTS =
-  "https://api.github.com/repos/reseau-constellation/iug/releases";
-export const URL_TÉLÉCHARGEMENTS =
-  "https://github.com/reseau-constellation/iug/releases";
+export const IPA_TÉLÉCHARGEMENTS = 'https://api.github.com/repos/reseau-constellation/iug/releases';
+export const URL_TÉLÉCHARGEMENTS = 'https://github.com/reseau-constellation/iug/releases';
